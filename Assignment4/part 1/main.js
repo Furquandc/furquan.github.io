efa0d466060812f4387578ef6bd58e1f5ead5400
Furquandc <144745137+Furquandc@users.noreply.github.com>
2023-11-21 15:41:49 -0500

    Somewhat completed the external JS file
    Added event listener, completed the first if statement, wrote code to ensure the paragraph changes every time the button is clicked

--- Assignment4/part 1/main.js
+++ Assignment4/part 1/main.js
@@ -1,8 +1,8 @@
 /*
-	Name: Furquan Abdurrahman
-	File: main.js
-	Date: 14 November 2023
-	Assignment 4 part 1
+    Name: Furquan Abdurrahman
+    File: main.js
+    Date: 14 November 2023
+    Assignment 4 part 1
 */
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
@@ -11,5 +11,30 @@ let insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
 let inserty = ["the soup kitchen", "Disneyland", "the White House"];
 let insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
 
+randomize.addEventListener('click', result);
 
+function result() {
 
+    let newStory = storyText;
+    let xItem = randomValueFromArray();
+    let yItem = randomValueFromArray();
+    let zItem = randomValueFromArray();
+    newStory.replaceAll('insertx', 'xItem');
+    newStory.replaceAll('inserty', 'yItem');
+    newStory.replaceAll('insertz', 'zItem');
+    newStory = newStory
+
+    if (customName.value !== '') {
+        const name = customName.value;
+        newStory.replaceAll('Bob', name);
+    }
+
+    if (document.getElementById("uk").checked) {
+        const weight = Math.round(300);
+        const temperature = Math.round(94);
+
+    }
+
+    story.textContent = newStory;
+    story.style.visibility = 'visible';
+}
