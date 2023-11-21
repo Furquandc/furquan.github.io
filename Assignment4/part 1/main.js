f5f738695c883f22c451c72850bb33f723438213
Furquandc <144745137+Furquandc@users.noreply.github.com>
2023-11-21 15:32:28 -0500

    Embedded JS and external JS
    Added the embedded JS via the script tag, and added the variables to the external JS file

--- Assignment4/part 1/main.js
+++ Assignment4/part 1/main.js
@@ -4,3 +4,12 @@
 	Date: 14 November 2023
 	Assignment 4 part 1
 */
+
+let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
+
+let insertx = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+let inserty = ["the soup kitchen", "Disneyland", "the White House"];
+let insertz = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+
+
+
