1a27293d4a5a1bc87801c38661715f6b0d0eb9fc
Furquandc <144745137+Furquandc@users.noreply.github.com>
2023-11-22 23:27:57 -0500

    Update main.js

--- Assignment4/part 2/gallery-start/gallery-start/main.js
+++ Assignment4/part 2/gallery-start/gallery-start/main.js
@@ -12,14 +12,17 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const imgArray = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
 /* Declaring the alternative text for each image file */
+const altText = { pic1: 'Close-up view of an eye', pic2: 'Cool painting', pic3: 'Purple and white flowers', pic4: 'Drawing of ancient Egypt', pic5: 'Butterfly on a large leaf' };
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (let i = 0; i < imgArray.length; i++) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', imgArray[i]);
+    newImage.setAttribute('alt', Object.key{i});
+    thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
